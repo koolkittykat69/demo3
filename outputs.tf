9595bcb000db110fdf17fe3c4e7b160436c017ac
koolkittykat69 <mazurov.artem99@gmail.com>
2022-01-17 11:09:17 +0200

    more unused

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-#output "available_avail_zones" {
-#  value = [for name in data.aws_availability_zones.available.names : name]
-#}
-
-#output "selected_avail_zones" {
-#  value = [for zone in var.avail_zones : zone]
-#}
-
-#output "nats_ips" {
-#  value = [for nat in aws_eip.eip : nat.public_ip]
-#}
-
 output "lb_ip" {
   value = aws_lb.test.dns_name
 }
