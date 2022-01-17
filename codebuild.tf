859fcadde7841f33610bea3df1ce12c60dafb946
koolkittykat69 <mazurov.artem99@gmail.com>
2022-01-17 11:02:02 +0200

    delete unused file

--- codebuild.tf
+++
@@ -1,6 +0,0 @@
-#resource "aws_codebuild_project" "final-demo" {
-#  name = "final-demo"
-#  build_timeout = 5
-#  queued_timeout = 5
-#  service_role = aws_iam_role.codebuild-app-final-demo.arn
-#}
